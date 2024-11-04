af4415026091dcfaf98aec2a1d96e918ce83e53d
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-04 19:32:16 +0100

    Update Header.jsx

--- alx-react-app - new/src/components/Header.jsx
+++ alx-react-app - new/src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-      <header style={{ backgroundColor: 'red', color: 'white', textAlign: 'center' }}>
+    header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>
    <h1>My Favorite Cities</h1>
  </header>
     );
