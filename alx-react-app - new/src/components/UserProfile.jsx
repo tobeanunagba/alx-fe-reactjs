a27c0d1a135daf398b02fe46bbc96b2bf5b24752
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-04 20:22:44 +0100

    Update UserProfile.jsx

--- alx-react-app - new/src/components/UserProfile.jsx
+++ alx-react-app - new/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ const UserProfile = (props) => {
       <div style={{ border: '1px solid gray', padding: '20px', margin: '15px', borderRadius: '8px' }}>
       <h2 style={{ color: 'blue', fontSize: '1.5em' }}>{props.name}</h2>
       <p>Age: <span style={{ fontWeight: 'bold', fontSize: '1.2em' }}>{props.age}</span></p>
-     <p>Bio: <span style={{ color: 'darkslategray' }}>{props.bio}</span></p>
+     <p> Bio:{props.bio}</span></p>
     </div>
     );
   };
