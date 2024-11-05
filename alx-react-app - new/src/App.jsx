fc1a97ff995a918ece9b8ff387df826cf55245fd
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-05 20:22:31 +0100

    Update App.jsx

--- alx-react-app - new/src/App.jsx
+++ alx-react-app - new/src/App.jsx
@@ -1,17 +1,19 @@
-import WelcomeMessage from './components/WelcomeMessage';
+
 import Header from './components/Header'
-import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
+import MainContent from './components/Maincontent';
+import Footer from './components/Footer'
 import Counter from './components/Counter';
 function App() {
   return (
     <>
       <div>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+       <Header />
+       <WelcomeMessage />
+        <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+       <Footer />
       <h1 style={{ textAlign: 'center' }}>Counter Application</h1>
       <Counter />
       </div>
