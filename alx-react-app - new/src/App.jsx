ca2803cfd097e5335406ff2d4bd1dd80eef63452
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-05 20:27:00 +0100

    Update App.jsx

--- alx-react-app - new/src/App.jsx
+++ alx-react-app - new/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import Header from './components/Header'
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-       <Header />
+        <Header />
        <WelcomeMessage />
         <MainContent />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
