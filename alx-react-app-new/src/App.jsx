1797ef048c454eaf1d316a33b9cda771c9b6c6ea
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 16:05:43 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './Components/Footer';
 import { useState } from 'react';
 
 function Counter(){
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <>
     <div>
