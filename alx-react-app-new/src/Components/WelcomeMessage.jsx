1cc8adcbfd5970c9679168cada4768b9fd2cfabe
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 16:33:57 +0100

    Update WelcomeMessage.jsx

--- alx-react-app-new/src/Components/WelcomeMessage.jsx
+++ alx-react-app-new/src/Components/WelcomeMessage.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 function WelcomeMessage() {
     return (
-        <div  style= {{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+        <div>
             <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX</p>
+            <p>I am learning about JSX!</p>
         </div>
     );
 }
