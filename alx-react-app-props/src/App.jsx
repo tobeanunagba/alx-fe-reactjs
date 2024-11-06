b1288c1c1a9109408d2c35769eed96ec17a16288
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 17:56:48 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,11 @@
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import { UserProvider } from './UserContext';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
   return (
-    <UserContext.Provider value={userData}>
+    <UserProvider>
       <ProfilePage />
-    </UserContext.Provider>
+    </UserProvider>
   );
 }
 
