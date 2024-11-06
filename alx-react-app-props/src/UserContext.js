ee3779904bcdc1260134e5bf337cddce4612398f
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 18:06:52 +0100

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,21 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext } from 'react';
 
 // Initialize the context
 const UserContext = createContext();
-
-// Create a provider component
-export function UserProvider({ children }) {
-  // Set up any default user data here or keep it flexible
-  const [userData, setUserData] = useState({
-    name: "Jane Doe",
-    email: "jane.doe@example.com"
-  });
-
-  return (
-    <UserContext.Provider value={userData}>
-      {children}
-    </UserContext.Provider>
-  );
-}
 
 export default UserContext;
