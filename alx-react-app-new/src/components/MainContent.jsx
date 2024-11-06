3f682d39548f5ba52a8bc9da290db9a77fa1fd41
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 16:39:03 +0100

    Add files via upload

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+
+function MainContent() {
+    return (
+      <main style= {{ padding: '20px', lineHeight: '1.6', color: 'darkslateblue', backgroundColor: '#f4f4f4' }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
