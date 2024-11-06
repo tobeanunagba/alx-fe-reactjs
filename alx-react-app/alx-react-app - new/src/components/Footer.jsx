a9c9f68c308f1e507a9408133e639f66f3b16867
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 14:52:41 +0100

    Add files via upload

---
+++ alx-react-app/alx-react-app - new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+    return (
+      <footer style= {{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
