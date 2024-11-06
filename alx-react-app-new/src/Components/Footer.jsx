017e23614d7ec00359130af9f4554d604c1c7a37
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 16:38:43 +0100

    Delete alx-react-app-new/src/Components directory

--- alx-react-app-new/src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center' }}>
-        <p>© 2023 City Lovers</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
--- alx-react-app-new/src/Components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-    return (
-      <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-        <p>I love to visit New York, Paris, and Tokyo.</p>
-      </main>
-    );
-  }
-  
-  export default MainContent;
