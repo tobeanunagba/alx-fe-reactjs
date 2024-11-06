fa523b0bbaf7aeb32eed3510a638958d99ee09c8
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 15:57:53 +0100

    Add files via upload

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,27 @@
+import WelcomeMessage from "./Components/WelcomeMessage";
+import Header from "./Components/Header";
+import MainContent from "./Components/MainContent";
+import UserProfile from "./Components/UserProfile";
+import Footer from "./Components/Footer";
+import { useState} from "react";
+
+function Counter(){
+  const [count, setCount] = useState(0)
+  return (
+    <>
+    <div>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <p>Current Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <Footer />
+    </div>
+    </>
+  );
+}
+
+export default Counter;
