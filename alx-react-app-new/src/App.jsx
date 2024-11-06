cdd1310a5b57b08f8702ab8cf58891cf917f2607
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 16:29:43 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,17 +1,32 @@
-import Header from "./components/Header";
-import UserProfile from "./components/UserProfile";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
+
+    <>
+      <div>
+      <WelcomeMessage />
       <Header />
-      <UserProfile name="Jane Doe" age={28} bio="Avid traveler and city explorer" />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <p>Current Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      
       <Footer />
     </div>
+    </>
   );
 }
 
-export default App;
+export default Counter
