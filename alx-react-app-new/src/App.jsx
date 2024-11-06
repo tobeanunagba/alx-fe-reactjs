8fc8da5cdb956503bbe08b1293caa013defec01c
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 16:19:52 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
