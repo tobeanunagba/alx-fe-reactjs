b8fab8e6e31bf236101c229cd1eb3106d14cd4f3
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-06 15:07:29 +0100

    Update App.jsx

--- alx-react-app - new/src/App.jsx
+++ alx-react-app - new/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
-      
       <Counter />
       </div>
     </>
