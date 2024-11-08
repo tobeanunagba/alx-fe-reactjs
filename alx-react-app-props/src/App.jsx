f2bd9d69dacd1bb9872709e79746ba46a4c4e15c
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-08 11:57:50 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProfilePage from './ProfilePage';
+import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext';
 
 function App() {
