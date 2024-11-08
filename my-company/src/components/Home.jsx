dfc528f7a4fc762bc318fe08ae297b91a0591257
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-08 16:41:19 +0100

    Add files via upload

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+      <div style={{ padding: '20px', textAlign: 'center' }}>
+        <h1>Welcome to Our Company</h1>
+        <p>We are dedicated to delivering excellence in all our services.</p>
+        <img src="/images/company-building.jpg" alt="Company Building" style={{ width: '100%', maxWidth: '600px', marginTop: '20px' }} />
+      </div>
+    );
+  }
+  
+  export default Home;
+  
