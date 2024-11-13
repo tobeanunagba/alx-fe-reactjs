854ebb480648252f5320f405d65266821d6afc4b
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-13 15:55:18 +0100

    Add files via upload

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
