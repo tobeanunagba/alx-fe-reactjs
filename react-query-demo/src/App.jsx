99b8438c77752307bc41560ac23557cf24089e00
tobechukwu Richmond <117280867+tobeanunagba@users.noreply.github.com>
2024-11-19 18:08:21 +0100

    Add files via upload

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
+
---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,35 @@
+// src/components/PostsComponent.jsx
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+  
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
